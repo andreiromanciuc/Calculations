f9edfd3562c029a091fdde531a7d91df9c729b7f
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2019-12-08 11:12:32 +0200

    Chapter 2 - Ex5

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -5,7 +5,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        SelfCheckout selfCheckout = new SelfCheckout();
-        selfCheckout.subTotal();
+      CurrencyConversion currencyConversion = new CurrencyConversion();
+      currencyConversion.conversion();
     }
 }
