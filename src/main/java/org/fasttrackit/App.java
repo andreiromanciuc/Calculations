0d664d49a76549f9bc922973c262779031840112
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2019-12-16 20:07:18 +0200

    Chapter 2 - Ex"Multi State Sales"

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -5,7 +5,7 @@ public class App
 {
     public static void main( String[] args )
     {
-      BmiCalculator bmiCalculator = new BmiCalculator();
-      bmiCalculator.bmiCalculations();
+      MultiStateSalesTaxCalculator multiStateSalesTaxCalculator = new MultiStateSalesTaxCalculator();
+      multiStateSalesTaxCalculator.calculate();
     }
 }
