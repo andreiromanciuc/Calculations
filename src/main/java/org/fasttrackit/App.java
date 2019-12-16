6ed4a85cdc002af19b070eb94ebeaab64cb86069
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2019-12-16 20:55:21 +0200

    Chapter 2 - Ex"Comparing Numbers"

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -5,7 +5,7 @@ public class App
 {
     public static void main( String[] args )
     {
-      NumbersToName numbersToName = new NumbersToName();
-      numbersToName.numbersToName();
+      ComparingNumbers comparingNumbers = new ComparingNumbers();
+      comparingNumbers.comparingNumbers();
     }
 }
