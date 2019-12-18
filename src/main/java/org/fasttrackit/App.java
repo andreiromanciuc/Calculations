8bebc32cbd76462951a1a490cbb90b87b7eeac80
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2019-12-18 21:17:22 +0200

    CozaLozaWoza... Sum pair and unpair numbers from a range

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,11 +1,27 @@
 package org.fasttrackit;
 
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-      TroubleshootingCarIssues troubleshootingCarIssues = new TroubleshootingCarIssues();
-      troubleshootingCarIssues.trouble();
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        SumEvenAndOdd sumEvenAndOdd = new SumEvenAndOdd();
+
+
+        System.out.println("How many time you want to repeat?");
+        Scanner scanner1 = new Scanner(System.in);
+        int t = scanner1.nextInt();
+
+        if (t <= 200 && t >= 1) {
+            for (int i = 0; i < t; i++) {
+                sumEvenAndOdd.sum();
+                System.out.println(" ");
+            }
+        }else {
+            System.out.println("introduce number from the range 1-200");
+
+        }
     }
+
 }
+
